0c3953b7d796aa34d9e817bca0c884017ddae2dd
therunningagent <luistjr@gmail.com>
2021-04-14 09:37:54 -0700

    finished problem 1

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,4 +1,23 @@
-window.addEventListener("keydown", (e) => {
-    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio)
-});
+const keys = document.querySelectorAll('.key');
+
+window.addEventListener("keydown", playSound)
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    key.classList.add('playing');
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+};
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+
+    this.classList.remove('playing');
+};
